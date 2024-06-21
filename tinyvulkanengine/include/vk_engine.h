ebefe672ff5653eef08d7d9e85304d24e9014fa1
vroja035 <105022916+vroja035@users.noreply.github.com>
2024-06-20 17:46:51 -0700

    setting up vulkan commands

--- tinyvulkanengine/include/vk_engine.h
+++ tinyvulkanengine/include/vk_engine.h
@@ -5,8 +5,17 @@
 
 #include <vk_types.h>
 
+struct FrameData {
+
+	VkCommandPool _commandPool;
+	VkCommandBuffer _mainCommandBuffer;
+};
+
+constexpr unsigned int FRAME_OVERLAP = 2;
+
 class VulkanEngine {
 public:
+
 
 	bool _isInitialized{ false };
 	int _frameNumber {0};
@@ -41,6 +50,13 @@ public:
 	std::vector<VkImage> _swapchainImages;
 	std::vector<VkImageView> _swapchainImageViews;
 	VkExtent2D _swapchainExtent;
+
+	FrameData _frames[FRAME_OVERLAP];
+
+	FrameData& get_current_frame() { return _frames[_frameNumber % FRAME_OVERLAP]; };
+
+	VkQueue _graphicsQueue;
+	uint32_t _graphicsQueueFamily;
 
 private:
 
